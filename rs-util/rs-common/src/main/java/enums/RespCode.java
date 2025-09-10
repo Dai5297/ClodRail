c0ec8b0c1c0747a0b4ac342224c9b42104a4b1df
dai <15160255297@163.com>
2025-09-10 19:58:56 +0800

    初始化基础工程结构

---
+++ rs-util/rs-common/src/main/java/enums/RespCode.java
@@ -0,0 +1,34 @@
+package enums;
+
+public enum RespCode {
+
+    SUCCESS(200, "操作成功"),
+    ERROR(500, "操作失败"),
+    SYSTEM_ERROR(501, "系统错误");
+
+    private Integer code;
+    private String message;
+
+    RespCode(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    RespCode() {}
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ rs-util/rs-common/src/main/java/util/RespResult.java
@@ -0,0 +1,57 @@
+package util;
+
+import enums.RespCode;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class RespResult<T> implements Serializable {
+
+    private T data;
+
+    private RespCode code;
+
+    private String message;
+
+    public RespResult(T data, RespCode code) {
+        this.data = data;
+        this.code = code;
+        this.message = code.getMessage();
+    }
+
+    public RespResult(RespCode code) {
+        this.code = code;
+        this.message = code.getMessage();
+    }
+
+    public RespResult() {
+    }
+
+    public static <T> RespResult<T> ok() {
+        return new RespResult<>(null, RespCode.SUCCESS);
+    }
+
+    public static <T> RespResult<T> ok(T data) {
+        return new RespResult<>(data, RespCode.SUCCESS);
+    }
+
+    public static <T> RespResult<T> error() {
+        return new RespResult<>(null, RespCode.ERROR);
+    }
+
+    public static <T> RespResult<T> error(String message) {
+        return secByError(message);
+    }
+
+    public static <T> RespResult<T> secByError(String message) {
+        RespResult<T> respResult = new RespResult<>();
+        respResult.setCode(RespCode.ERROR);
+        respResult.setMessage(message);
+        return respResult;
+    }
+
+    public static <T> RespResult<T> error(RespCode respCode) {
+        return new RespResult<>(respCode);
+    }
+}
